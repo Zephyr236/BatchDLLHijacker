7140818ebed0975d0cfed993481202b595f25a3d
Zephyr236 <yhovtu@foxmail.com>
2026-01-23 19:53:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
 
 
 with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-    for i in range(0,10):
+    for i in range(0,1000):
         # get_download_link("https://www.snapfiles.com/php/surpriseme.php")
         executor.submit(get_download_link,"https://www.snapfiles.com/php/surpriseme.php")
 
